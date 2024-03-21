ba0b8132667fa6fe0105bffae659d708c4d9b8a1
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-03-21 09:41:06 -0500

    get-all-products working

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
-[
+const store = [
     {
-        "name": "apple",
+        "productName": "apple",
         "price": 1.5
     }
 ]
@@ -11,11 +11,20 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res)=>{
-    res.json({message: 'you are at the route'})
+    res.json(store)
 })
 
+router.get('/get-all-products', (req, res)=>{
+    let allProductName = []
+    for(let obj of store){
+        allProductName.push(obj.productName)
+    }
+    res.json(allProductName)
+})
 
+// router.get('/get-product:productName', (req, res){
 
+// })
 
 
 
