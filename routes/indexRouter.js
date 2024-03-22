93117e86c3d93a69e6c52be1f75d66ee55c85970
Samantha Pomeroy <samanthapomeroy38@gmail.com>
2024-03-22 08:44:39 -0500

    initial commit

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,6 +1,6 @@
 const store = [
     {
-        "productName": "apple",
+        "name": "apple",
         "price": 1.5
     }
 ]
@@ -15,17 +15,49 @@ router.get('/', (req, res)=>{
 })
 
 router.get('/get-all-products', (req, res)=>{
-    let allProductName = []
+    let allProducts = []
     for(let obj of store){
-        allProductName.push(obj.productName)
+        allProducts.push(obj.name)
     }
-    res.json(allProductName)
+    res.json(allProducts)
 })
 
-// router.get('/get-product:productName', (req, res){
+router.get('/get-product/:name', (req, res)=>{
+    let search = req.params
+    let itemSearch
+    for(let item of store){
+        console.log(search.name)
+        if(search.name === item.name){
+            itemSearch = item.name
+        }
+    }
+    if(!itemSearch){
+        res.end("item not found")
+    }else{
+        res.json(itemSearch)
+    }
+})
 
-// })
-
-
+router.get("/create-product", (req, res)=>{
+    let existingItem
+    for(let object of store){
+        if(object.name === req.body.name){
+            existingItem = true
+            break
+        }
+    }
+    if(existingItem){
+        res.end("existing item")
+    }else{
+        store.push({
+            name:req.body.name,
+            price:req.body.price
+        })
+        res.json({
+            data:store,
+            message:"Product added"
+        })
+    }
+})
 
 module.exports = router
